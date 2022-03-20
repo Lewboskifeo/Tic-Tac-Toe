2c2c34a2b2c5aa7547916cab5f7721506427022f
Tomas <tomasborquez13@gmail.com>
2022-03-20 08:34:47 -0300

    some small changes to tic-tac-toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,3 +1,5 @@
+import sys
+
 theBoard = {'0': ' ', '1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': ' ', '8': ' '}
 notes = {'tl': '0', 'tm': '1', 'tr': '2', 'ml': '3', 'mm': '4', 'mr': '5', 'bl': '6', 'bm': '7', 'br': '8'}
 
@@ -15,14 +17,14 @@ def pick():
     return T
     
 def win():
-    global W, CW, L, p1, p3
+    global W, L, P1, P3
     L = "X"
     for n in range(2):
         for i in range(3):
-            if theBoard.get(str(p3), "") == L and theBoard.get(str(p3 + 1), "") == L and theBoard.get(str(p3 + 2), "") == L:
+            if theBoard.get(str(P3), "") == L and theBoard.get(str(P3 + 1), "") == L and theBoard.get(str(P3 + 2), "") == L:
                 W += 1
                 break
-            elif theBoard.get(str(0 + p1), "") == L and theBoard.get(str(3 + p1), "") == L and theBoard.get(str(6 + p1), "") == L:
+            elif theBoard.get(str(0 + P1), "") == L and theBoard.get(str(3 + P1), "") == L and theBoard.get(str(6 + P1), "") == L:
                 W += 1
                 break
             elif theBoard.get(str(0), "") == L and theBoard.get(str(4), "") == L and theBoard.get(str(8), "") == L:
@@ -31,34 +33,34 @@ def win():
             elif theBoard.get(str(6), "") == L and theBoard.get(str(4), "") == L and theBoard.get(str(2), "") == L:
                 W += 1
                 break
-            p1 += 1
-            p3 += 3
-        p1 = 0    
-        p3 = 0
+            P1 += 1
+            P3 += 3
+        P1 = 0    
+        P3 = 0
         L = "O"
     return W
 
 def loop():
-    global TT, T, v, W, WW, fboard
+    global TT, T, V, W, WW, FBOARD
     pick()
-    fboard = notes.get(T, 0)
-    for v in range(1):
+    FBOARD = notes.get(T, 0)
+    for V in range(1):
         try:
-            if theBoard[fboard] == "X":
+            if theBoard[FBOARD] == "X":
                 loop()
-            elif theBoard[fboard] == "O":
+            elif theBoard[FBOARD] == "O":
                 loop()
             else:
                 break
         except:
             loop()
             break
-    theBoard[fboard] = TT
+    theBoard[FBOARD] = TT
     win()
     babyboard()
     if W >= 1:
         print(">> The player ", TT, " WON!!!", sep="")
-        exit()
+        sys.exit()
     if TT == "X":
         TT = "O"
     else:
@@ -67,13 +69,12 @@ def loop():
 
 TT = "X"
 T = ""
-v = 0
+V = 0
 W = 0
 WW = ""
-fboard = 0
+FBOARD = 0
 
-p1 = 0
-p3 = 0
-CW = 0
+P1 = 0
+P3 = 0
 L = ""
 loop()
